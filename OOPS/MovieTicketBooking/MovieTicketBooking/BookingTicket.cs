edaa52a9f626e91fb69cc08c290a458b685f48d8
shreyadakhale200 <shreyadakhale200@gmail.com>
2023-04-21 18:22:48 +0530

    Program added

--- OOPS/MovieTicketBooking/MovieTicketBooking/BookingTicket.cs
+++ OOPS/MovieTicketBooking/MovieTicketBooking/BookingTicket.cs
@@ -23,7 +23,7 @@ namespace MovieTicketBooking
 
     class OnlineBooking : movieInfo
     {
-        //public double discount = 20;
+        public double discount = 0.2;
 
         public override double MovieTicket(string movie_name , double ticket_price, int number_ofTickets)
         {
@@ -80,17 +80,73 @@ namespace MovieTicketBooking
                     ticket_Price = 10;
             }
             ticket_Price = number_ofTickets * ticket_Price;
+            double discountAmount = ticket_price * discount;
+            ticket_price = ticket_price - discountAmount;
             return ticket_Price;
         }
     }
 
     class BoxOffice : movieInfo
     {
-        public double booking_fees = 40;
+        double booking_fees = 0.4;
 
         public override double MovieTicket(string movie_name , double ticket_price, int number_ofTickets)
         {
-            ticket_Price = number_ofTickets + ticket_Price + booking_fees;
+            if (movie_name == "Forest Gump")
+            {
+                if (theater_name == "Inox")
+                {
+                    ticket_Price = 400;
+                }
+                else if (theater_name == "Tuli")
+                {
+                    ticket_Price = 250;
+                }
+                else
+                    ticket_Price = 300;
+            }
+            else if (movie_name == "Interstellar")
+            {
+                if (theater_name == "Inox")
+                {
+                    ticket_Price = 560;
+                }
+                else if (theater_name == "Tuli")
+                {
+                    ticket_Price = 510;
+                }
+                else
+                    ticket_Price = 500;
+            }
+            else if (movie_name == "Rocket Boys")
+            {
+                if (theater_name == "Inox")
+                {
+                    ticket_Price = 210;
+                }
+                else if (theater_name == "Tuli")
+                {
+                    ticket_Price = 123;
+                }
+                else
+                    ticket_Price = 200;
+            }
+            else if (movie_name == "Martian")
+            {
+                if (theater_name == "Inox")
+                {
+                    ticket_Price = 40;
+                }
+                else if (theater_name == "Tuli")
+                {
+                    ticket_Price = 20;
+                }
+                else
+                    ticket_Price = 10;
+            }
+            ticket_Price = number_ofTickets * ticket_Price;
+            double bookingFeesAmount = ticket_price * booking_fees;
+            ticket_price = ticket_price + bookingFeesAmount;
             return ticket_Price;
         }
     }
--- OOPS/MovieTicketBooking/MovieTicketBooking/Form1.cs
+++ OOPS/MovieTicketBooking/MovieTicketBooking/Form1.cs
@@ -17,94 +17,88 @@ namespace MovieTicketBooking
             string movie_name = comboBox2.Text;
             string theater_name = comboBox3.Text;
             DateTime show_Time = Convert.ToDateTime(dateTimePicker1.Text);
-            int seat_number = Convert.ToInt32(textBox4.Text);
+            int seat_number;
             double ticket_price = 0;
-            int number_ofTickets = Convert.ToInt32(textBox3.Text);
-
             movieInfo mi = null;
-            mi = new OnlineBooking();
-            if (comboBox2.Text == "Forest Gump")
-            {
-                mi = new OnlineBooking();
-            }
-            else if (comboBox2.Text == "Interstellar")
-            {
-                mi = new OnlineBooking();
-            }
-            else if (comboBox2.Text == "Rocket Boys")
-            {
-                mi = new OnlineBooking();
 
-            }
-            else if (comboBox2.Text == "Martian")
+            if (comboBox1.Text == "Online Booking")
             {
-                mi = new OnlineBooking();
-            }
-
-            TableLayoutPanel tableLayoutPanel1 = new TableLayoutPanel();
-            tableLayoutPanel1.RowCount = 18;
-            tableLayoutPanel1.ColumnCount = 10;
-            this.Controls.Add(tableLayoutPanel1);
-
-            CheckBox[,] seats = new CheckBox[tableLayoutPanel1.RowCount, tableLayoutPanel1.ColumnCount];
-            for (int row = 0; row < tableLayoutPanel1.RowCount; row++)
-            {
-                for (int col = 0; col < tableLayoutPanel1.ColumnCount; col++)
+                if (comboBox2.Text == "Forest Gump")
                 {
-                    CheckBox seat = new CheckBox();
-                    seat.Text = "Seat " + (row * tableLayoutPanel1.ColumnCount + col + 1).ToString();
-                    seats[row, col] = seat;
-                    tableLayoutPanel1.Controls.Add(seat, col, row);
-                    bool allChecked = true;
-
-                    foreach (Control control in this.Controls)
-                    {
-                        if (control is CheckBox)
-                        {
-                            CheckBox checkbox = (CheckBox)control;
-
-                            if (!checkbox.Checked)
-                            {
-                                allChecked = false;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (allChecked)
-                    {
-                        // All checkboxes are checked
-                    label4.Text = (seat).ToString();
-                    }
-                    //else
-                    //{
-                        // Not all checkboxes are checked
-                    //}
+                    mi = new OnlineBooking();
+                }
+                else if (comboBox2.Text == "Interstellar")
+                {
+                    mi = new OnlineBooking();
+                }
+                else if (comboBox2.Text == "Rocket Boys")
+                {
+                    mi = new OnlineBooking();
 
                 }
+                else if (comboBox2.Text == "Martian")
+                {
+                    mi = new OnlineBooking();
+                }
             }
-    DateTime dt = dateTimePicker1.Value;
-    string format = "yyyy/MM/dd HH:mm:ss";
-    mi.MovieTicket(movie_name, ticket_price, number_ofTickets);
+            else if (comboBox1.Text == "Box Office Booking")
+            {
+                if (comboBox2.Text == "Forest Gump")
+                {
+                    mi = new BoxOffice();
+                }
+                else if (comboBox2.Text == "Interstellar")
+                {
+                    mi = new BoxOffice();
+                }
+                else if (comboBox2.Text == "Rocket Boys")
+                {
+                    mi = new BoxOffice();
+
+                }
+                else if (comboBox2.Text == "Martian")
+                {
+                    mi = new BoxOffice();
+                }
+            }
+            int count = 0;
+            List<string> checkedCheckboxes = new List<string>();
+            label5.Text = "Your seats are = ";
+            for (int i = 1; i <= 408; i++)
+            {
+                string checkboxName = "Checkbox" + i.ToString();
+                CheckBox checkbox = this.Controls.Find(checkboxName, true).FirstOrDefault() as CheckBox;
+
+                if (checkbox != null && checkbox.Checked)
+                {
+                    checkedCheckboxes.Add(checkboxName);
+                }
+            }
+
+            foreach (string checkboxName in checkedCheckboxes)
+            {
+                string nameWithoutCheckbox = checkboxName.Replace("Checkbox", ""); // remove "Checkbox" from the name
+                label5.Text += nameWithoutCheckbox + " ";
+                count++;
+            }
+            int number_ofTickets = count;
+            DateTime dt = dateTimePicker1.Value;
+            string format = "yyyy/MM/dd HH:mm:ss";
+            mi.MovieTicket(movie_name, ticket_price, number_ofTickets);
             label4.Text = Environment.NewLine;
             label4.Text += "Movie : " + movie_name + Environment.NewLine;
             label4.Text += "Theatre : " + theater_name + Environment.NewLine;
             //label4.Text += show_Time.ToString() + Environment.NewLine;
             label4.Text += "Date and Time : " + dt.ToString(format) + Environment.NewLine;
-            //label4.Text += "Seat Number : " + seat_number.ToString() + Environment.NewLine;
+            label4.Text += "Number of Seats : " + count.ToString() + Environment.NewLine;
             //label4.Text = tableLayoutPanel1.Controls.Add(seat, col, row);
-            
+
             label4.Text += "Total Price : " + mi.ticket_Price.ToString();
-
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
         }
     }
 }
