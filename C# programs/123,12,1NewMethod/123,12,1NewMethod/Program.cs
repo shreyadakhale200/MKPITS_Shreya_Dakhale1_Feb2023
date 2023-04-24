3ac39727d1484570ac3b0bcc643cee66320593c9
shreyadakhale200 <shreyadakhale200@gmail.com>
2023-04-24 11:21:58 +0530

    Program added

---
+++ C# programs/123,12,1NewMethod/123,12,1NewMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _123_12_1NewMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 2 3 
+            //1 2 
+            //1
+            int row, column;
+            for (row = 3; row >= 1; row--)
+            {
+                for (column = 1; row >= column; column++)
+                {
+                    Console.Write(column + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# programs/methodInOtherMethod/methodInOtherMethod/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodInOtherMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Poly students");
+            Poly();
+            Console.ReadKey();
+        }
+
+        static void Poly()
+        {
+            String[] Name = {"Shreya","Rupali","Vanshika"};
+            Console.WriteLine("BE students");
+            BE();
+            foreach (String s in Name)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        static void BE()
+        {
+            String[] Name = {"Mahima","Pallavi","Rashmi"};
+            foreach (String s in Name)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
