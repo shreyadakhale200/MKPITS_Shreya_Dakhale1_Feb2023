9be3b6a7e64c490e7813e50760902959ed0923c0
shreyadakhale200 <shreyadakhale200@gmail.com>
2023-04-26 00:45:23 +0530

    Programs Added

---
+++ OOPS/ModifyingInstanceVariables/ModifyingInstanceVariables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifyingInstanceVariables
+{
+    class Student
+    {
+        public int rollNo = 34;    // fields having public access modifier
+        public string Name = "Shreya";         // fields having public access modifier
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.rollNo = 54;    //Modifying Instance Variable
+        }
+    }
+}
