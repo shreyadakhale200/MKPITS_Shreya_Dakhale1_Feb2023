74232a1cbad82b2f23d1b645a2e3e412fd5cdedc
shreyadakhale200 <shreyadakhale200@gmail.com>
2023-04-26 17:14:30 +0530

    Programs Added

---
+++ OOPS/FinallyExceptionMethod/FinallyExceptionMethod/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinallyExceptionMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 0;
+
+            try
+            {
+                int res = num1 / num2;
+                Console.WriteLine("result = " + res);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("Finally Executed");
+            }
+            Console.WriteLine("BYe" + "\n");
+
+            int num3 = 12;
+            int num4 = 2;
+
+            try
+            {
+                int res = num3 / num4;
+                Console.WriteLine("result = " + res);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("Finally Executed Second time.");
+            }
+            Console.WriteLine("Bye Again");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOPS/SealedMethod1/SealedMethod1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedMethod1
+{
+    class Account
+    {
+        public string accountName;
+        public double Accountno;
+        public int bal = 34000; 
+
+        public virtual void CheckAcc()
+        {
+            Console.WriteLine("Account name : " + accountName);
+            Console.WriteLine("Account Number : " +  Accountno);
+        }
+    }
+    class Saving:Account
+    {
+        public sealed override void CheckAcc()
+        {
+            bal = bal + 300;
+            Console.WriteLine(bal);
+        }
+    }
+    class Current:Saving
+    {
+        //public override void CheckAcc()
+        //{
+            //Current.CheckAcc() cannot override inherited member 'Saving.CheckAcc()' because it is sealed
+        //}
+        public void display()
+        {
+            bal = bal - 3000;
+            Console.WriteLine(bal);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saving s = new Saving();
+            s.CheckAcc();
+
+            Current c = new Current();
+            c.display();
+
+            Console.ReadKey();
+        }
+    }
+}
