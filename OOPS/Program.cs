722ef912b930edc86ac3c7bc8176eeae6a43ee17
shreyadakhale200 <shreyadakhale200@gmail.com>
2023-07-14 21:31:55 +0530

    OOPS Programs Added

---
+++ OOPS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region==========Object Creation==========
+            ///<summary>
+            ///Creating an instance of the class Named RollNo_1
+            /// </summary>
+            StandardFifth RollNo_1 = new StandardFifth(1, "Shreya", "Dakhale", 22, "shreyadakhale200@gmail.com", "9970104869",89,87,65,90,99);
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
