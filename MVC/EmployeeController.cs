dcb98bffabb54432c2da613a28a55d924896113b
shreyadakhale200 <shreyadakhale200@gmail.com>
2023-07-14 21:33:43 +0530

    API Programs Added

---
+++ MVC/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using API_Webapplication_Creation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API_Webapplication_Creation.Models;
+
+namespace API_Webapplication_Creation.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        //Creating an array of objects in employee class
+        Employee[] employees = new Employee[]
+        {
+            new Employee{Id=1,Name="Mahima",JoiningDate = DateTime.Parse(DateTime.Today.ToString())}
+        }
+    }
+}
